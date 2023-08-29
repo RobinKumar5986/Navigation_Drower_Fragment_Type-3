ca7848c480c486a04d22417beed7af3492c8cb39
Robin Kumar <rkrobin6550@gmail.com>
2023-08-29 22:37:47 +0530

    Initial commit

---
+++ app/src/main/java/com/example/haider/navigation_drower_fragment_type_3/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.haider.navigation_drower_fragment_type_3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
